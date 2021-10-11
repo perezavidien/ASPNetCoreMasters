1f4b1e56f956b85d8200a1758bfd2ae1b0dd1914
Avi Perez <perezavidien@gmail.com>
2021-10-12 04:44:01 +0800

    Added ItemExistsFilter.cs

---
+++ ASPNetCoreMastersTodoList.Api/Filters/ItemExistsFilter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreMastersTodoList.Api.Filters
+{
+    public class ItemExistsFilter : IActionFilter
+    {
+        private IItemService _itemService;
+
+        public ItemExistsFilter(IItemService itemService)
+        {
+            _itemService = itemService;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        { }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            // call svc
+        }
+    }
+
+    public class ItemExistAttribute : TypeFilterAttribute
+    {
+        public ItemExistAttribute() : base(typeof(ItemExistsFilter))
+        { }
+    }
+
+}
